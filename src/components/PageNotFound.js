3d23c1455920056eeacd5c9f6e29c6bd444bca87
MrGrigoryAlexandrovich <ahmedcvrle@gmail.com>
2021-08-22 12:42:23 +0200

    add not found page with redirection to home after 5 seconds

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "../css/PageNotFound.css";
 class PageNotFound extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            seconds:5
-        };
-      }
-      componentDidMount() {
-        let timer = setInterval(() => {
-            if (this.state.seconds === 0) {
-                clearInterval(timer);
-                this.props.history.push('/')
-            }
-             else
-            this.setState({ seconds:this.state.seconds-1})
-          }, 1000);
-      }
-    render() {
-        return (
-            <div id="notfound">
-            <div className="notfound">
-              <div className="notfound-404">
-                <h1>404</h1>
-                <h2>PAGE NOT FOUND</h2>
-              </div>
-              <span className="text-light">REDIRECTION</span>
-              <div className="redirection text-light">{this.state.seconds}</div>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 5,
+    };
+  }
+  componentDidMount() {
+    let timer = setInterval(() => {
+      if (this.state.seconds === 0) {
+        clearInterval(timer);
+        this.props.history.push("/");
+      } else this.setState({ seconds: this.state.seconds - 1 });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>404</h1>
+            <h2>PAGE NOT FOUND</h2>
           </div>
-        )
-    }
+          <span className="text-light">REDIRECTION</span>
+          <div className="redirection text-light">{this.state.seconds}</div>
+        </div>
+      </div>
+    );
+  }
 }
 export default withRouter(PageNotFound);
